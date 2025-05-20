8000f4e422fb5c5e9e3dd4a6ca9ed06098d76f33
irenecarll <irenecarl.lewiss@gmail.com>
2025-05-20 10:31:21 +0700

    cart dan product page

---
+++ resources/views/components/product/search.blade.php
@@ -0,0 +1,22 @@
+<style>
+.products-section {
+    background-color: #fbe8d3;
+    padding: 40px;
+}
+
+.search-bar {
+    text-align: center;
+}
+
+.search-bar input[type="text"] {
+    width: 70%;
+    border-radius: 20px;
+    border: 1px solid #ccc;
+}
+</style>
+
+<section class="products-section">
+    <div class="search-bar">
+        <input type="text" placeholder="Search products...">
+    </div>
+</section>
